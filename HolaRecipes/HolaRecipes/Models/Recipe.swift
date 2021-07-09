dfbfad595469eb731b77cdbb730f2d0de5d60536
Aditi3 <aditi.gvc@gmail.com>
2021-07-09 12:42:25 +0530

    chore: Restructure Project and Create Groups

--- HolaRecipes/HolaRecipes/Models/Recipe.swift
+++ HolaRecipes/HolaRecipes/Models/Recipe.swift
@@ -13,11 +13,10 @@ struct Recipe: Codable {
     let recipeDescription: String
     let difficulty: Int
     let headline, id, image, name: String
-    let fats: String?
 
     enum CodingKeys: String, CodingKey {
         case recipeDescription = "description"
-        case difficulty, headline, id, image, name, fats
+        case difficulty, headline, id, image, name
     }
 }
 
