5580ece993c8781ef8f859e6504c04c3f553708d
Aditi3 <aditi.gvc@gmail.com>
2021-07-09 12:40:18 +0530

    task: Adds Recipe Network Call and Parsing Logic

--- HolaRecipes/HolaRecipes/RecipeManager.swift
+++ HolaRecipes/HolaRecipes/RecipeManager.swift
@@ -7,4 +7,45 @@
 
 import Foundation
 
-
+/// Handles the request related to Recipe API
+class RecipeManager {
+    
+    let recipeUrl = "https://raw.githubusercontent.com/Aditi3/recipes/develop/recipes.json"
+    
+    // MARK: -  Fetch and Handle Recipe Request
+    
+    /// Fetch the Recipes data
+    ///
+    /// - Parameters:
+    ///   - success: returns the successfully fetched and parsed recipe list
+    ///   - failure: returns an failure/ error message
+    func fetchRecipes(success: @escaping ([Recipe]) -> (), failure: @escaping (Error) -> ()) {
+        
+        NetworkHandler().requestJSON(apiPath: recipeUrl, apiParameters: [:], success: { (successJson) in
+            let recipesData = self.parseRecipesJson(recipesData: successJson)
+            recipesData != nil ? success(recipesData!) : failure(APIError.parseError(""))
+        }, failure: {(error) in
+            failure(error)
+        })
+    }
+    
+    // MARK: - Parsing Recipes Json Response
+    
+    // MARK: Parse Recipes Json
+    
+    /// Parse the Recipes JSON
+    ///
+    /// - Parameters:
+    ///   - [Recipe]:  returns list of recipes
+    private func parseRecipesJson(recipesData: Data) -> [Recipe]? {
+        let decoder = JSONDecoder()
+        do {
+            let recipesDecodedData = try decoder.decode(Recipes.self, from: recipesData)
+            return recipesDecodedData
+            
+        } catch {
+            print("JSON error: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
--- HolaRecipes/HolaRecipes/ViewController.swift
+++ HolaRecipes/HolaRecipes/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        requestRecipes()
     }
-
-
+    
+    func requestRecipes() {
+        
+        RecipeManager().fetchRecipes { recipes in
+            for recipe in recipes {
+                print(recipe.name)
+                print("===================================")
+            }
+        } failure: { error in
+            print(error.localizedDescription)
+        }
+    }
+    
 }
 
